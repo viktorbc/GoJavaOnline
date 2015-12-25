cf6ede0d225260c9c65b75556d3424e9b86d2358
viktorbc <bcmagazine@ukr.net>
2015-12-25 16:39:39 +0200

    homework modul 5 number 2

---
+++ src/dz51/Bubblesort.java
@@ -0,0 +1,34 @@
+package dz51;
+
+/**
+ * Created by Виктор on 25.12.2015.
+ */
+public class Bubblesort {
+    public static void main(String args[]) {
+        // Инициализация массива
+        int nums[] = {55, 17, 100, 8, 37,
+                28, 320, 87, 65, 22};
+        int a, b, t;
+        int size = 10;
+        System.out.print("List of number array is: ");
+        for(int i = 0; i < size; i++)
+            System.out.print(" " + nums[i]);
+        System.out.println();
+
+
+        for(a = 1; a < size; a++)
+            for(b = size - 1; b >= a; b--) {
+                if(nums[b - 1] > nums[b]) {
+                    t = nums[b - 1];
+                    nums[b - 1] = nums[b];
+                    nums[b] = t;
+                }
+            }
+
+
+        System.out.print("Sorted list of number array is: ");
+        for(int i = 0; i < size; i++)
+            System.out.print(" " + nums[i]);
+            System.out.println();
+    }
+}
