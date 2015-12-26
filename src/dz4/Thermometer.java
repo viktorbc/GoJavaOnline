7bf7bcecaa3ce0841bf3c0eab7cdb1a1348406ad
viktorbc <bcmagazine@ukr.net>
2015-12-26 13:38:54 +0200

    homework modul 4

--- src/dz4/Thermometer.java
+++ src/dz4/Thermometer.java
@@ -1,35 +1,15 @@
 package dz4;
-
-
     public class Thermometer {
-        private double c;
-        private double f;
+        static double f = 100;
+        static double c = 20;
         public static void main(String[] args) {
 
-            public static double getC()  {
-                return c;
-            }
 
-        public void setC(double c) {
-            this.c = c;
-        }
-
-        public double getF() {
-            return f;
-        }
-
-        public void setF(double f) {
-            this.f = f;
-        }
 
         double celsium = (f - 32) * 8 / 10;
-
-
         double farhengeit = 8 * c / 10 + 32;
-
-
         System.out.println(farhengeit + " grad Farhengeit have equivalent " +  celsium + " grad Celsium");
-        System.out.println(celsium + " grad Farhengeit have equivalent " + farhengeit + " grad Farhengeit");
+        System.out.println(celsium + " grad Celsium have equivalent " + farhengeit + " grad Farhengeit");
     }
 
 
