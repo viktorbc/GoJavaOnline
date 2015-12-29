b65ad563225dcaa854f11d8a9fb138dd20631670
viktorbc <bcmagazine@ukr.net>
2015-12-29 17:23:16 +0200

    homework modul 6

---
+++ src/dz61/Audioplayer.java
@@ -0,0 +1,34 @@
+package dz61;
+
+import Exemples.NegativeAge;
+
+import java.lang.*;
+import java.util.Scanner;
+import java.lang.String;
+
+/**
+ * Created by Виктор on 29.12.2015.
+ */
+public class Audioplayer {
+    public static void main(String args[]) throws java.lang.IllegalStateException {
+
+        int[] Arrey_Album = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(" Enter  number track ");
+        final Scanner scanner = new Scanner(System.in);
+        final String numberTrack = scanner.next();
+        try {
+            final int track = Integer.parseInt(numberTrack);
+            if (track <= 10) {
+                System.out.println("  your choice track number " + track);
+            } else {
+                throw new IllegalStateExceptionTrack(track);
+            }
+        } catch (NumberFormatException ex) {
+            System.out.printf(" Error: - this track is not in album! ");
+        } catch (IllegalStateExceptionTrack e) {
+            System.out.printf(" Error: - this track is not in album " + e.getTrackValue() + " track should be 1 to 9 ");
+        }
+
+
+    }
+}
