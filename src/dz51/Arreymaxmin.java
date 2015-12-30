87ffb77a84f8b7e5329a414ed7c58aaa44037799
viktorbc <bcmagazine@ukr.net>
2015-12-30 17:26:02 +0200

    homework modul 41

--- src/dz51/Arreymaxmin.java
+++ src/dz51/Arreymaxmin.java
@@ -3,33 +3,26 @@ public class Arreymaxmin {
     public static void main(String[] args) {
 
         int[] Arrey_mn = {10, 5, 3, 2, 6, 20, 7, 4};
-        int max = 0, min = Arrey_mn[0];
+        int maximum = 0, minimum = Arrey_mn[0];
         for (int i = 0; i < 8; i++) {
-            if (Arrey_mn[i] < max) {
-                max = max;
+            if (Arrey_mn[i] < maximum) {
+                maximum = maximum;
             } else {
-                max = Arrey_mn[i];
+                maximum = Arrey_mn[i];
             }
-            if (Arrey_mn[i] > min) {
-                min = min;
-            }
-            else {
-                min = Arrey_mn[i];
-            }
+
         }
-
-
         for (int i = 0; i < 8; i++) {
-            if (Arrey_mn[i] > min) {
-                min = min;
+            if (Arrey_mn[i] > minimum) {
+                minimum = minimum;
             }
             else {
-                min = Arrey_mn[i];
+                minimum = Arrey_mn[i];
             }
 
         }
-        System.out.println("Max number " + max);
-        System.out.println("Min number " + min);
+        System.out.println("Max number " + maximum);
+        System.out.println("Min number " + minimum);
     }
 }
 
