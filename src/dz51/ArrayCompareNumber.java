081954a116ba551b470e5f8a35b587abc8404198
viktorbc <bcmagazine@ukr.net>
2016-01-02 18:50:37 +0200

    HOMEWORK2016

--- src/dz51/ArrayCompareNumber.java
+++ src/dz51/ArrayCompareNumber.java
@@ -7,7 +7,7 @@ public class ArrayCompareNumber {
     public static void main(String[] args) {
 
         int[] Array_mn = {10, 5, 3, 2, 6, 20, 7, 4};
-        int ArrayMaximum = 0;
+        int ArrayMaximum = Array_mn[0];
         for (int i = 0; i < Array_mn.length - 1 ; i++) {
             if (ArrayMaximum < Array_mn[i]) {
                 ArrayMaximum = Array_mn[i];
