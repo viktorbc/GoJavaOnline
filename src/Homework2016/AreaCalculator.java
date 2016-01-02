fe8e1ff3ace274042990fd34750919b08d8a6fec
viktorbc <bcmagazine@ukr.net>
2016-01-02 16:48:01 +0200

    HOMEWORK2016

--- src/Homework2016/AreaCalculator.java
+++ src/Homework2016/AreaCalculator.java
@@ -4,15 +4,15 @@ package Homework2016;
  * Created by Виктор on 02.01.2016.
  */
 public class AreaCalculator {
-    public double ArreaCalculatorRectangle(double width, double height){
+    public double arreaCalculatorRectangle(double width, double height){
         return width * height;
     }
 
-    public double ArreaCalculatorTriangle(double width, double height){
+    public double arreaCalculatorTriangle(double width, double height){
         return width * height / 2;
     }
 
-    public double ArreaCalculatorCircle(double radius){
+    public double arreaCalculatorCircle(double radius){
 
         return (Math.pow(radius, 2) * Math.PI);
     }
