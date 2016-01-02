d923d10fada415d236ce5915bc11832530226ccb
viktorbc <bcmagazine@ukr.net>
2016-01-02 16:40:37 +0200

    HOMEWORK2016

---
+++ src/Homework2016/CelsiumToFarhengeit.java
@@ -0,0 +1,10 @@
+package Homework2016;
+
+/**
+ * Created by Виктор on 02.01.2016.
+ */
+public class CelsiumToFarhengeit {
+    public double CelsiumToFarhengeit (double cm){
+        return (cm * 5 / 9) + 32;
+    }
+}
