32af74e87afee048deb886e03dffca81692b690f
viktorbc <bcmagazine@ukr.net>
2016-01-02 16:16:19 +0200

    HOMEWORK4

---
+++ src/Homework4/AreaCalculator.java
@@ -0,0 +1,22 @@
+package Homework4;
+
+import static java.lang.Math.PI;
+
+/**
+ * Created by Виктор on 02.01.2016.
+ */
+public class AreaCalculator {
+    public double ArreaCalculatorRectangle(double width, double height){
+        return width * height;
+    }
+
+    public double ArreaCalculatorTriangle(double width, double height){
+        return width * height / 2;
+    }
+
+    public double ArreaCalculatorCircle(double radius){
+
+        return radius * radius * PI;
+    }
+
+}
---
+++ src/Homework4/DistancePoint.java
@@ -0,0 +1,13 @@
+package Homework4;
+
+/**
+ * Created by Виктор on 02.01.2016.
+ */
+public class DistancePoint {
+    double Length(double x1, double x2, double y1, double y2) {
+        double z = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
+        return Math.sqrt(z);
+    }
+}
+
+
