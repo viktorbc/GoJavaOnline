309dd16e859975e89ec84f644adf76d7110a5645
viktorbc <bcmagazine@ukr.net>
2016-01-02 16:46:28 +0200

    HOMEWORK2016

--- src/Homework2016/FarhengeitToCelsium.java
+++ src/Homework2016/FarhengeitToCelsium.java
@@ -5,12 +5,12 @@ package Homework2016;
  */
 public class FarhengeitToCelsium {
 
-        public double FahrenheitToCelsium(double ft){
+        public double fahrenheitToCelsium(double ft){
             return (ft-32)*5/9;
         }
 
     public class CelsiumToFarhengeit {
-        public double CelsiumToFarhengeit (double cm){
+        public double celsiumToFarhengeit (double cm){
             return (cm * 5 / 9) + 32;
         }
     }
