7345ea24fababc992b5b6a0d4d4c6edd5442fef2
viktorbc <bcmagazine@ukr.net>
2016-01-03 14:01:38 +0200

    HOMEWORK2016

--- src/Homework2016/BubleSorting.java
+++ src/Homework2016/BubleSorting.java
@@ -4,28 +4,16 @@ package Homework2016;
  * Created by Виктор on 02.01.2016.
  */
 public class BubleSorting {
-    public void bubbleSort(int nums[]) {
+    public void bubbleSort(int nums[], int temp) {
 
-            System.out.print("List of number array is: ");
-         for (int i = 0; i < nums.length - 1; i++)
-            System.out.print(" " + nums[i]);
-            System.out.println();
-
-         for (int a = 1; a < nums.length - 1; a++)
-                for (int b = nums.length - 1; b >= a; b--) {
-                    if (nums[b - 1] > nums[b]) {
-                        int t = nums[b - 1];
-                        nums[b - 1] = nums[b];
-                        nums[b] = t;
-                    }
+        for (int a = 1; a < nums.length - 1; a++)
+            for (int b = nums.length - 1; b >= a; b--) {
+                if (nums[b - 1] > nums[b]) {
+                    temp = nums[b - 1];
+                    nums[b - 1] = nums[b];
+                    nums[b] = temp;
                 }
-            System.out.print("Sorted list of number array is: ");
-            for (int i = 0; i < nums.length - 1; i++)
-                System.out.print(" " + nums[i]);
-            System.out.println();
-        }
-
+            }
     }
-
-
+}
 
