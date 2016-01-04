b18ceb8bbea3c653618fac1b4c90b6bfe7b97af5
viktorbc <bcmagazine@ukr.net>
2016-01-04 19:13:21 +0200

    HOMEWORK2016

--- src/Homework2016/ArrayCompareNumber.java
+++ src/Homework2016/ArrayCompareNumber.java
@@ -4,30 +4,37 @@ package Homework2016;
  * Created by Виктор on 02.01.2016.
  */
 public class ArrayCompareNumber {
-    public  void arrayCompare(int[] Array_mn, int ArrayMaximum, int ArrayMinimum) {
-
-
-        for (int i = 0; i < Array_mn.length - 1 ; i++) {
-            if (ArrayMaximum < Array_mn[i]) {
-                ArrayMaximum = Array_mn[i];
+    public void arrayCompareNumberMax(int arr[]) {
+        int maxNumber = 0;
+        for (int i = 0; i < arr.length - 1 ; i++) {
+            if (arr[i + 1] < arr[i]) {
+                arr[i] = arr[i + 1] + arr[i];
+                arr[i + 1] = arr[i] - arr[i + 1];
+                arr[i] = arr[i] - arr[i + 1];
+                maxNumber = arr[i + 1];
             }
             else {
-                ArrayMaximum = ArrayMaximum;
+                maxNumber = arr[i];
             }
         }
-
-
-        for (int i = 0; i < Array_mn.length - 1 ; i++) {
-            if (ArrayMinimum > Array_mn[i]) {
-                ArrayMinimum = Array_mn[i];
-            }
-            else {
-                ArrayMinimum = ArrayMinimum;
-            }
-        }
-
     }
+    public void arrayCompareNumberMin(int arr[]) {
+        int minNumber = 0;
+        for (int i = 0; i < arr.length - 1 ; i++) {
+            if (arr[i] < arr[i + 1]) {
+                arr[i + 1] = arr[i + 1] + arr[i];
+                arr[i] = arr [i + 1] - arr[i];
+                arr[i + 1] = arr[i + 1] - arr[i];
+                minNumber = arr[i + 1];
+            }
+            else {
+                minNumber = arr[i];
+            }
+        }
+    }
+
 }
+
 
 
 
