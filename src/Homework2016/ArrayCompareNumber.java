108c79847ae890da953562e00870fc0b5b5afe38
viktorbc <bcmagazine@ukr.net>
2016-01-06 09:50:56 +0200

    HOMEWORK2016

--- src/Homework2016/ArrayCompareNumber.java
+++ src/Homework2016/ArrayCompareNumber.java
@@ -4,21 +4,29 @@ package Homework2016;
  * Created by Виктор on 02.01.2016.
  */
 public class ArrayCompareNumber {
-    public void arrayCompareNumberMax(int arr[]) {
-        int maxNumber = arr[0], minNumber = arr[0];
-        for (int i = 0; i < arr.length - 1 ; i++) {
-            if( maxNumber < arr[i])
-                maxNumber = arr[i];
-            if ( minNumber > arr[i]) {
-                minNumber = arr[i];
-            }
-            else {
-                minNumber = minNumber;
-            }
-        }
+       public static void arrayCompareNumber(int arr[]) {
+           int maxNumber = arr[0], minNumber = arr[0];
+           for (int i = 0; i <= arr.length - 1; i++) {
+               if (maxNumber < arr[i]) {
+                   maxNumber = arr[i];
+               }
+               if (minNumber > arr[i]) {
+                   minNumber = arr[i];
+               }
+           }
+           System.out.println(" Max number " + maxNumber);
 
+           System.out.println(" Min number " + minNumber);
+
+           System.out.println();
+       }
+
+    public static void main(String[] args) {
+        int[] tempArray = {1, 2, 3, -10, 0, -20};
+        arrayCompareNumber(tempArray);
     }
 }
+
 
 
 
